c4db20b16fff8a24d4c7725709871566b0bbb070
Daiane Vieira <daiane.vieira07@yahoo.com.br>
2020-05-06 15:19:15 -0300

    Ordenar Funcionando

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { example } from './data.js';
+import { orderPokes } from './data.js';
 
 import data from './data/pokemon/pokemon.js';
 
@@ -21,8 +21,8 @@ var scrollTop = function (evt) {
 };
 document.getElementById("btn-top").addEventListener("click", scrollTop);
 
-function pokemonImage(numeroDoIdDoPersonagem) {
-   const way = data.pokemon[numeroDoIdDoPersonagem];
+function pokemonImage(numeroDoIdDoPersonagem, banco) {
+   const way = banco[numeroDoIdDoPersonagem];
    let box = `
       <div class="column pokemon" id="pokemon${way.id}" data-id="${way.id}" >
       <img src=${way.img}>
@@ -35,19 +35,19 @@ function criaImagemNoHtml() {
    let htmlCards = '';
    for (let i = 0; i < data.pokemon.length; i++) {
 
-      htmlCards += pokemonImage(i)
+      htmlCards += pokemonImage(i, data.pokemon)
    }
-   document.getElementById("filtered").innerHTML += htmlCards; 
+   document.getElementById("div-pokes").innerHTML += htmlCards;
 }
 criaImagemNoHtml();
 const pokemons = document.getElementsByClassName("pokemon");
-function rodarModal(){
-for (const pokemon of pokemons) {
-   pokemon.addEventListener("click", function () {
-      const id = pokemon.getAttribute("data-id");
-      abrirModal(id - 1)
-   });
-}
+function rodarModal() {
+   for (const pokemon of pokemons) {
+      pokemon.addEventListener("click", function () {
+         const id = pokemon.getAttribute("data-id");
+         abrirModal(id -1)
+      });
+   }
 }
 rodarModal();
 function abrirModal(index) {
@@ -64,52 +64,32 @@ function abrirModal(index) {
       <p class:"text">Evolução Anterior:${caminho.prev_evolution ? caminho.prev_evolution[0].name : "Não tem evolução"}</p>
       <p class:"text">Proxima Evolução:${caminho.next_evolution ? caminho.next_evolution[0].name : "Não tem evolução"}</p>
       </div>`
-      let modal = document.getElementById("myModal");
-      let span = document.getElementsByClassName("close")[0]; 
-      modal.style.display = "block";
-      document.getElementById("modal1").innerHTML = box2;
-      span.onclick = function () {
+   let modal = document.getElementById("myModal");
+   let span = document.getElementsByClassName("close")[0];
+   modal.style.display = "block";
+   document.getElementById("modal1").innerHTML = box2;
+   span.onclick = function () {
       modal.style.display = "none";
-      }
-      window.onclick = function (event) {
+   }
+   window.onclick = function (event) {
       if (event.target == modal) {
          modal.style.display = "none";
       }
    }
    return box2;
 }
-//console.log(example, data);
 
-
-//função ordenar Dai
-  /*
-const orderAZ = (a, b) => a["name"].localeCompare(b["name"])
-const orderByHeight = (a, b) => Number(a["height"].split(" ")[0]) - Number(b["height"].split(" ")[0])
-const orderSpawnChance = (a, b) => Number(a["order-spawn"]) - Number(b["order-spawn"])
-
-var searchOrder = document.getElementById("order-search").value
-var order = searchOrder.value
-
-document.getElementById("order-search").onchange = function() {orderSea()};
-function orderSea() {
-
-            orderPokes(order)
-      criaImagemNoHtml(pokemons);
+var searchOrder = document.getElementById("order-search")
+document.getElementById("order-search").onchange = function () {
+   const ordenada = orderPokes(searchOrder.value)
+   const htmlCards = document.getElementById("div-pokes")
+   htmlCards.innerHTML = ""
+   let template =""
+   for (let i = 0; i < data.pokemon.length; i++) {
+      template += pokemonImage(i, ordenada)
    }
+   htmlCards.innerHTML = template
+   rodarModal()
+}
 
 
-function orderPokes(data, order) {
-   switch (order) {
-      case "size":
-         data.sort(orderByHeight())
-         break
-
-      case "order-sprawn":
-         data.sort(orderSpawnChance())
-         break
-      case "order-az":
-         data.sort(orderAZ())
-         console.log(pokemons.sort(orderAZ))
-         break
-   }
-} */
