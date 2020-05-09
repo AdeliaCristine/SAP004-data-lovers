75439a3b547c3acb2b951c6186aae4951f32af69
Daiane Vieira <daiane.vieira07@yahoo.com.br>
2020-05-09 11:31:18 -0300

    Order Z-A

--- src/data.js
+++ src/data.js
@@ -21,29 +21,28 @@ export function selectFilter(search) {
 // função filtrar por fraqueza NOVA
 export function selectWeak(search) {
   let weakFilter = document.getElementById("filter-weakness").value;
-  return way.filter(function (search) { 
+  return way.filter(function (search) {
     return search.weaknesses.includes(weakFilter)
 
   });
 }
 //função ordenar
 let orderAZ = (a, b) => (a["name"]).localeCompare(b["name"])
+let orderZA = (a, b) => (a["name"]).localeCompare(b["name"])
 let orderByHeight = (a, b) => Number(a["height"].split(" ")[0]) - Number(b["height"].split(" ")[0])
 let orderSpawnChance = (a, b) => Number(a["spawn_chance"]) - Number(b["spawn_chance"])
 export function orderPokes(order) {
   switch (order) {
     case "size":
       return way.sort((a, b) => orderByHeight(a, b))
-
       break
-
     case "order-spawn":
       return way.sort((a, b) => orderSpawnChance(a, b))
-
       break
     case "order-az":
       return way.sort((a, b) => orderAZ(a, b))
-
       break
- }
+    case "order-za":
+      return way.sort((a, b) => orderZA(a, b)).reverse();
+  }
 }
